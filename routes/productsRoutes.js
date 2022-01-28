d89f1bbb4d5f04cc2af711eb460fb1f0e9c1baaf
mmunhz <mmunhz.ufabc@gmail.com>
2022-01-28 18:54:13 -0300

    Add save to database method using Mongoose

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,10 +5,10 @@ const ProductController = require('../controllers/ProductController')
 
 router.get('/create', ProductController.createProduct)
 router.post('/create', ProductController.createProductPost)
-router.post('/remove/:id', ProductController.removeProduct)
-router.post('/edit/', ProductController.editProductPost)
-router.get('/edit/:id', ProductController.editProduct)
-router.get('/:id', ProductController.getProduct)
-router.get('/', ProductController.showProducts)
+// router.post('/remove/:id', ProductController.removeProduct)
+// router.post('/edit/', ProductController.editProductPost)
+// router.get('/edit/:id', ProductController.editProduct)
+// router.get('/:id', ProductController.getProduct)
+// router.get('/', ProductController.showProducts)
 
 module.exports = router
